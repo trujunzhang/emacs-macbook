49f35568f77f0416833a9ad7c5ac9b560728c74f
Trujun Zhang <trujunzhang@gmail.com>
2016-01-19 21:09:43 +0800

    add multi-team.

---
+++ modules/prelude-project.el
@@ -0,0 +1,25 @@
+
+;; ==================================================
+;;                 HELM mode settings
+;; ==================================================
+(require 'helm-config)
+
+(setq make-backup-files nil)
+
+;; Change C-x with C-n and C-c with C-i on Colemak layout
+;; (keyboard-translate ?\C-n ?\C-x)
+;; (keyboard-translate ?\C-x ?\C-n)
+;; (keyboard-translate ?\C-i ?\C-c)
+;; (keyboard-translate ?\C-c ?\C-i)
+
+;; Cursor keys on home row
+(global-set-key (kbd "M-e") 'next-line)
+(global-set-key (kbd "M-u") 'previous-line)
+(global-set-key (kbd "M-n") 'backward-char)
+(global-set-key (kbd "M-i") 'forward-char)
+
+(projectile-global-mode)
+
+
+(provide 'prelude-project)
+;; prelude-project.el ends here.
