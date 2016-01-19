2d8c8b11d3964fb76b3562acaba5d1abaf4977ee
Trujun Zhang <trujunzhang@gmail.com>
2016-01-19 21:41:08 +0800

    add multi-team.

--- modules/prelude-ido.el
+++ modules/prelude-ido.el
@@ -1,9 +1,19 @@
 
+;; Ido
+
+;; Ido mode provides a nice way to navigate the filesystem. This is mostly just turning it on.
 
 (require 'ido)
 (ido-mode t)
+(setq ido-enable-flex-matching t
+      ido-use-virtual-buffers t)
 
+;; Column number mode
 
+;; Turn on column numbers.
+
+(setq column-number-mode t)
 
 
 (provide 'prelude-ido)
+;; prelude-ido.el ends here.
